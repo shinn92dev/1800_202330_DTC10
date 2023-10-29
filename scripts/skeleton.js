0370cb9c2355783851a4fde886db1183193674bb
Edro Gonzales <egonzales15@my.bcit.ca>
2023-10-29 13:14:49 -0700

    feat (login): create skeleton components

---
+++ scripts/skeleton.js
@@ -0,0 +1,5 @@
+function loadSkeleton() {
+    console.log($("#NAVBAR-PLACEHOLDER").load("./components/navbar.html"));
+    console.log($("#FOOTER-PLACEHOLDER").load("./components/footer.html"));
+}
+loadSkeleton();
