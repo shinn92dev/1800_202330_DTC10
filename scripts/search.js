a76e6df37d8b9acdd51c3ce05c5304b591b6fd7d
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-10-30 16:51:49 -0700

    add (search): add searchBarLogic function

--- scripts/search.js
+++ scripts/search.js
@@ -1,12 +1,22 @@
-const searchLogic = () => {
+function searchBarLogic() {
     const searchIcon = document.querySelectorAll(".header__icon")[1];
     const searchInput = document.querySelector(".header__search-bar > div");
     const h1 = document.querySelector("h1");
+    const closeIcon = document.querySelector(".close__icon")
 
     searchIcon.addEventListener("click", function () {
         searchInput.classList.toggle("mobile-hidden");
         h1.classList.toggle("mobile-hidden");
+        searchIcon.classList.toggle("mobile-hidden");
+        closeIcon.classList.toggle("mobile-hidden")
     });
-};
 
-export { searchLogic };
+    closeIcon.addEventListener("click", function () {
+        searchInput.classList.toggle("mobile-hidden");
+        h1.classList.toggle("mobile-hidden");
+        searchIcon.classList.toggle("mobile-hidden");
+        closeIcon.classList.toggle("mobile-hidden");
+    });
+}
+
+export { searchBarLogic };
