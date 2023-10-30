014b8117d6e702a84c74bea0c86ecf2c87217ec7
Edro Gonzales <egonzales15@my.bcit.ca>
2023-10-30 16:38:41 -0700

    update (template): modularize logic

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,5 +1,9 @@
-const navIcon = document.querySelectorAll(".header__icon")[0];
-const nav = document.querySelector("nav");
-navIcon.addEventListener("click", function () {
-    nav.classList.toggle("mobile-hidden");
-});
+function navbarLogic() {
+    const navIcon = document.querySelectorAll(".header__icon")[0];
+    const nav = document.querySelector("nav");
+    navIcon.addEventListener("click", function () {
+        nav.classList.toggle("mobile-hidden");
+    });
+}
+
+export { navbarLogic };
--- scripts/search.js
+++ scripts/search.js
@@ -1,8 +1,12 @@
-const searchIcon = document.querySelectorAll(".header__icon")[1];
-const searchInput = document.querySelector(".header__search-bar > div");
-const h1 = document.querySelector("h1");
+const searchLogic = () => {
+    const searchIcon = document.querySelectorAll(".header__icon")[1];
+    const searchInput = document.querySelector(".header__search-bar > div");
+    const h1 = document.querySelector("h1");
 
-searchIcon.addEventListener("click", function () {
-    searchInput.classList.toggle("mobile-hidden");
-    h1.classList.toggle("mobile-hidden");
-});
+    searchIcon.addEventListener("click", function () {
+        searchInput.classList.toggle("mobile-hidden");
+        h1.classList.toggle("mobile-hidden");
+    });
+};
+
+export { searchLogic };
