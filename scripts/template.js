f36dabd0ae11aa80363b22c741525551df6b63a7
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-10-30 17:18:22 -0700

    modify(template): modify listings.html and template.js

--- scripts/template.js
+++ scripts/template.js
@@ -1,9 +1,10 @@
+
 import { navbarLogic } from "./nav";
 import { searchBarLogic } from "./search";
 
 function initializeNav(callback) {
     $("#NAVBAR-PLACEHOLDER").load("./components/navbar.html", function () {
-        navbarLogic();
+        navbarLogic()
     });
 }
 
