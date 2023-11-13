fa4f8b4c661016d9aae6d83adcb891a6e3ead3eb
Edro <edrogonzales@hotmail.com>
2023-11-13 12:10:15 -0800

    feat (fuzzy-search): search by postal code

--- scripts/listings.js
+++ scripts/listings.js
@@ -1,85 +1,101 @@
 function getListingDataAndDisplay() {
     $(document).ready(function () {
-        const matchingListingLst = [];
-
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
-        const searchParam = urlParams.get("search").trim();
-
+        const searchParam = urlParams.get("search")?.trim();
         const ul = $("#property-ul");
 
-        db.collection("Properties")
+        if (!searchParam) {
+            const noResultsMessage = generateNoResultsMessage();
+            ul.append(noResultsMessage);
+            return;
+        }
+
+        const matchingListingSet = new Set();
+
+        const searchByAddress = db
+            .collection("Properties")
             .where("propertyFullAddress", ">=", searchParam)
             .where("propertyFullAddress", "<=", searchParam + "\uf8ff")
+            .get();
+
+        const searchByPostalCode = db
+            .collection("Properties")
             .where("postalCode", ">=", searchParam)
             .where("postalCode", "<=", searchParam + "\uf8ff")
-            .get()
-            .then((querySnapshot) => {
-                if (querySnapshot.empty) {
-                    console.log("No matching documents.");
-                    const listItem = $(`<div class="listings_message">
-                    <p class="listings_message-p">Seems like this property does not exist...</p>
-                    <a href="./add-property.html" class="listings_message-a">Add this property & leave a review</a>
-                </div>`);
-                    ul.append(listItem);
-                    return;
-                }
+            .get();
 
-                querySnapshot.forEach((doc) => {
-                    matchingListingLst.push(doc.data());
-                    console.log(doc.id, " => ", doc.data());
-                });
+        Promise.all([searchByAddress, searchByPostalCode])
+            .then((results) => {
+                let noResults = true;
 
-                // Display data
-                $.each(matchingListingLst, function (_, listing) {
-                    // create html element variables
-                    const listItem = $(
-                        '<li class="property-card d-flex justify-content-between align-items-center"></li>'
-                    );
-                    const scoreBox = $(
-                        '<div class="property-card__score-box"><div class="d-flex justify-content-center align-items-center"></div></div>'
-                    );
-                    scoreBox.find("div").text(listing.overallScore.toFixed(1));
-                    console.log(scoreBox);
-                    const textBox = $(
-                        '<div class="property-card__text-box d-flex flex-column"></div>'
-                    );
-                    const topTextBox = $(
-                        '<div class="d-flex justify-content-between align-items-center text-box__top"><h2></h2><img src="./images/bookmark.svg" alt="" /></div>'
-                    );
-                    topTextBox.find("h2").text(listing.type);
-                    const middleTextBox = $(
-                        '<div class="text-box__middle"><h3></h3></div>'
-                    );
-                    address =
-                        listing.propertyFullAddress.replace(/_/g, " ") +
-                        ", " +
-                        listing.postalCode;
-                    middleTextBox.find("h3").text(address);
-                    const bottomTextBox = $(
-                        '<div class="text-box__bottom d-flex justify-content-around"></div>'
-                    );
-
-                    tagLst = listing.tags;
-                    tagLst.forEach((tag) => {
-                        pTag = $("<p></p>");
-                        pTag.text(tag);
-                        bottomTextBox.append($(pTag));
+                results.forEach((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        const docData = doc.data();
+                        if (!matchingListingSet.has(doc.id)) {
+                            matchingListingSet.add(doc.id);
+                            const listingItem = generateListingItem(docData);
+                            ul.append(listingItem);
+                            noResults = false;
+                        }
                     });
-
-                    textBox.append(topTextBox);
-                    textBox.append(middleTextBox);
-                    textBox.append(bottomTextBox);
-                    listItem.append(scoreBox);
-                    listItem.append(textBox);
-                    console.log(listItem);
-                    ul.append(listItem);
                 });
+
+                if (noResults) {
+                    const noResultsMessage = generateNoResultsMessage(true);
+                    ul.append(noResultsMessage);
+                }
             })
             .catch((error) => {
                 console.error("Error getting documents: ", error);
             });
     });
+}
+
+function generateListingItem(listing) {
+    const tags = listing.tags.map((tag) => `<p>${tag}</p>`).join("");
+    const address = `${listing.propertyFullAddress.replace(/_/g, " ")}, ${
+        listing.postalCode
+    }`;
+
+    return `
+        <li class="property-card d-flex justify-content-between align-items-center">
+            <div class="property-card__score-box">
+                <div class="d-flex justify-content-center align-items-center">
+                    ${listing.overallScore.toFixed(1)}
+                </div>
+            </div>
+            <div class="property-card__text-box d-flex flex-column">
+                <div class="text-box__middle">
+                    <h3>${address}</h3>
+                </div>
+                <div class="text-box__bottom d-flex justify-content-around">
+                    ${tags}
+                </div>
+            </div>
+            <div class="d-flex justify-content-between align-items-center text-box__top">
+                <img src="./images/bookmark.svg" alt="" />
+            </div>
+        </li>
+    `;
+}
+
+function generateNoResultsMessage(hasResults = false) {
+    const message = hasResults
+        ? "Seems like this property does not exist..."
+        : "Nothing to search for...";
+    const linkText = hasResults
+        ? "Add this property & leave a review"
+        : "Go back";
+
+    return `
+        <div class="listings_message">
+            <p class="listings_message-p">${message}</p>
+            <a href="./${
+                hasResults ? "add-property" : "index"
+            }.html" class="listings_message-a">${linkText}</a>
+        </div>
+    `;
 }
 
 getListingDataAndDisplay();
