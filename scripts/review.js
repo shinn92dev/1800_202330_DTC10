5a6fe44b9f9e8501227ffcc9c3000c3f1e7d00da
Anthony Shin <shinn92dev@gmail.com>
2023-11-15 20:42:20 -0800

    feat (review): save review form data to Firestore(Review collection)

--- scripts/review.js
+++ scripts/review.js
@@ -220,9 +220,18 @@ function formatTag(str) {
 
     return finalTag;
 }
+
+function calcAverageFromObj(obj) {
+    const lst = Object.values(obj);
+    let total = 0;
+    lst.forEach((number) => {
+        total += number;
+    });
+    return total / lst.length;
+}
 function getFormData() {
     const resultObj = {
-        user: window.localStorage.getItem("userUID"),
+        userId: window.localStorage.getItem("userUID"),
         eachScore: {
             cleanliness: 0,
             houserule: 0,
@@ -230,14 +239,20 @@ function getFormData() {
             location: 0,
             price: 0,
         },
+        overallScore: 0,
         tags: [],
-        comment: "",
+        review: "",
+        propertyId: "",
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
     };
     window.localStorage.removeItem("userUID");
+    let total = 0;
     scoresInputs.forEach((score) => {
         const key = score.id.split("-")[0];
         resultObj["eachScore"][key] = Number(score.value);
+        total += Number(score.value);
     });
+    resultObj["overallScore"] = total / 5;
     tags.forEach((tag) => {
         const targetInput = tag.previousElementSibling;
         if (targetInput.checked) {
@@ -247,10 +262,16 @@ function getFormData() {
             }
         }
     });
-    resultObj["comment"] = document
+    resultObj["review"] = document
         .querySelector("#form-comment-box textarea")
         .value.trim();
     return resultObj;
+}
+
+function storeReviewFormDataToFirestore(resultObj) {
+    var reviewRef = db.collection("Reviews");
+    console.log("save function");
+    reviewRef.add(resultObj);
 }
 
 function validateForm(e) {
@@ -266,6 +287,7 @@ function validateForm(e) {
         e.preventDefault();
         const formData = getFormData();
         console.log(formData);
+        storeReviewFormDataToFirestore(formData);
     } else {
         e.preventDefault();
         makeWarningToInvalidScoreBox(scoreBoxObj);
