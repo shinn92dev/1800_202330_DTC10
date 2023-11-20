a07c01e59dad8504d6f83f52b3e647ddbf23e7e8
Edro <edrogonzales@hotmail.com>
2023-11-19 21:03:39 -0800

    feat (bookmark): add bookmark for listings.js + add bookmark history page

--- scripts/listings.js
+++ scripts/listings.js
@@ -1,3 +1,17 @@
+let currentUser;
+
+const getUser = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            currentUser = db.collection("Users").doc(user.uid);
+        } else {
+            console.log("No user is signed in");
+        }
+    });
+};
+
+getUser();
+
 function getListingDataAndDisplay() {
     $(document).ready(function () {
         const queryString = window.location.search;
@@ -38,6 +52,20 @@ function getListingDataAndDisplay() {
                             const listingItem = generateListingItem(docData);
                             ul.append(listingItem);
                             noResults = false;
+
+                            const saveButton = $(`#save-${doc.id}`);
+                            saveButton.on("click", () =>
+                                updateBookmark(doc.id)
+                            );
+
+                            currentUser.get().then((userDoc) => {
+                                const bookmarks = userDoc.data().bookmarks;
+                                if (bookmarks.includes(doc.id)) {
+                                    document.getElementById(
+                                        "save-" + doc.id
+                                    ).innerText = "bookmark";
+                                }
+                            });
                         }
                     });
                 });
@@ -53,31 +81,61 @@ function getListingDataAndDisplay() {
     });
 }
 
+function updateBookmark(docID) {
+    currentUser.get().then((userDoc) => {
+        console.log("userDoc: ", userDoc.data());
+        const bookmarks = userDoc.data().bookmarks;
+        const isBookmarked = bookmarks.includes(docID);
+        const iconID = "save-" + docID;
+        const firestoreFieldValue = firebase.firestore.FieldValue;
+
+        currentUser
+            .update({
+                bookmarks: isBookmarked
+                    ? firestoreFieldValue.arrayRemove(docID)
+                    : firestoreFieldValue.arrayUnion(docID),
+            })
+            .then(() => {
+                console.log(
+                    isBookmarked
+                        ? "Item was removed from bookmarks: "
+                        : "This item was added to bookmarks: ",
+                    docID
+                );
+                document.getElementById(iconID).innerText = isBookmarked
+                    ? "bookmark_border"
+                    : "bookmark";
+            });
+    });
+}
+
 function generateListingItem(listing) {
     const tags = listing.tags.map((tag) => `<p>${tag}</p>`).join("");
     const address = `${listing.propertyFullAddress.replace(/_/g, " ")}, ${
         listing.postalCode
     }`;
+    const propertyID = listing.propertyID;
+
     return `
-        <li>
-            <a href="/details.html?propertyId=${
-                listing.propertyID
-            }" class="property-card d-flex justify-content-between align-items-center"><div class="property-card__score-box">
-                <div class="d-flex justify-content-center align-items-center">
-                    ${listing.overallScore.toFixed(1)}
+        <li class="d-flex justify-content-between align-items-center property-card">
+            <a href="/details.html?propertyId=${propertyID}" class="d-flex flex-grow-1 justify-content-between align-items-center">
+                <div class="property-card__score-box">
+                    <div class="d-flex justify-content-center align-items-center">
+                        ${listing.overallScore.toFixed(1)}
+                    </div>
                 </div>
-            </div>
-            <div class="property-card__text-box d-flex flex-column">
-                <div class="text-box__middle">
-                    <h3>${address}</h3>
+                <div class="property-card__text-box d-flex flex-column flex-grow-1">
+                    <div class="text-box__middle">
+                        <h3>${address}</h3>
+                    </div>
+                    <div class="text-box__bottom d-flex justify-content-around">
+                        ${tags}
+                    </div>
                 </div>
-                <div class="text-box__bottom d-flex justify-content-around">
-                    ${tags}
-                </div>
-            </div>
+            </a>
             <div class="d-flex justify-content-between align-items-center text-box__top">
-                <i style="cursor: pointer;" class="material-icons">bookmark_border</i>
-            </div></a>
+                <i id="save-${propertyID}" style="cursor: pointer;" class="material-icons">bookmark_border</i>
+            </div>
         </li>
     `;
 }
