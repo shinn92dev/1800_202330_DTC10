94271af97bc558c9680c95ab1d9cad8a0f6a6984
Anthony Shin <shinn92dev@gmail.com>
2023-12-08 00:16:27 -0800

    add (thankyou): add comments for thankyou.js

--- scripts/thankyou.js
+++ scripts/thankyou.js
@@ -1,3 +1,4 @@
+// Get propertyId from url, and set "propertyId" for a tag href attribute
 const propertyId = new URL(location.href).searchParams.get("propertyId");
 const link = document.querySelector(".thankyou-link");
 link.href = `/details.html?propertyId=${propertyId}`;
